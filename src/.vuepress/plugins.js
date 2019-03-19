2e36193f4594425f3ea114bf8f6ba861eca3e689
Daniel da Rocha <danrocha@gmail.com>
2019-03-19 13:07:16 +0100

    fix css

--- src/.vuepress/plugins.js
+++ src/.vuepress/plugins.js
@@ -1,44 +1,44 @@
 let plugins = [require('tailwindcss')('./tailwind.config.js'), require('cssnano')];
 
-if (process.env.NODE_ENV === 'production') {
-  // only add purgecss if production
-  plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        '.vuepress/theme/**/*.vue',
-        '.vuepress/theme/**/*.md',
-        '.vuepress/theme/**/*.js',
-        '.vuepress/theme/templates/*.html',
-        '.vuepress/theme/styles/*.css',
-        '.vuepress/components/*.vue'
-      ],
+// if (process.env.NODE_ENV === 'production') {
+//   // only add purgecss if production
+//   plugins.push(
+//     require('@fullhuman/postcss-purgecss')({
+//       content: [
+//         '.vuepress/theme/**/*.vue',
+//         '.vuepress/theme/**/*.md',
+//         '.vuepress/theme/**/*.js',
+//         '.vuepress/theme/templates/*.html',
+//         '.vuepress/theme/styles/*.css',
+//         '.vuepress/components/*.vue'
+//       ],
 
-      extractors: [
-        {
-          /**
-           * A fix for purge css to pick up class names with escaped chars
-           * E.g. md:w-1/2.
-           *
-           * Solution from https://github.com/tailwindcss/tailwindcss/issues/391#issuecomment-366922730
-           */
-          extractor: class TailwindExtractor {
-            static extract(content) {
-              return content.match(/[A-z0-9-:\/]+/g) || [];
-            }
-          },
-          extensions: ['css', 'html', 'js', 'vue', 'md'],
-        },
-      ],
+//       extractors: [
+//         {
+//           /**
+//            * A fix for purge css to pick up class names with escaped chars
+//            * E.g. md:w-1/2.
+//            *
+//            * Solution from https://github.com/tailwindcss/tailwindcss/issues/391#issuecomment-366922730
+//            */
+//           extractor: class TailwindExtractor {
+//             static extract(content) {
+//               return content.match(/[A-z0-9-:\/]+/g) || [];
+//             }
+//           },
+//           extensions: ['css', 'html', 'js', 'vue', 'md'],
+//         },
+//       ],
 
-      /**
-       * Ensure default resets and normalised classes are not removed by PurgeCSS
-       */
-      whitelistPatterns: [
-        /^(h\d|p$|ul|li$|div|ol|table|td$|th$|thead|tbody|main|input|button|form|md-|hljs)/,
-      ],
-    })
-  );
-}
+//       /**
+//        * Ensure default resets and normalised classes are not removed by PurgeCSS
+//        */
+//       whitelistPatterns: [
+//         /^(h\d|p$|ul|li$|div|ol|table|td$|th$|thead|tbody|main|input|button|form|md-|hljs)/,
+//       ],
+//     })
+//   );
+// }
 
 // always add autoprefixer at the end
 plugins.push(require('autoprefixer'));
