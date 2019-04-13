5288da88f95a9b352ca6b4c36aa1182ee632ae1a
Daniel da Rocha <danrocha@gmail.com>
2019-04-13 12:54:45 +0200

    fix: fathos to danroc.dev

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -165,16 +165,16 @@ module.exports = {
     [
       'script', {},
       `(function(f, a, t, h, o, m){
-            a[h] = a[h] || function () {
-              (a[h].q = a[h].q || []).push(arguments)
-            };
-          o=f.createElement('script'),
-          m=f.getElementsByTagName('script')[0];
-          o.async=1; o.src=t; o.id='fathom-script';
-          m.parentNode.insertBefore(o,m)
-        })(document, window, '//stats.theforeignarchitect.com/tracker.js', 'fathom');
-        fathom('set', 'siteId', 'JIHKT');
-        fathom('trackPageview');`
+        a[h]=a[h]||function(){
+          (a[h].q=a[h].q||[]).push(arguments)
+        };
+        o=f.createElement('script'),
+        m=f.getElementsByTagName('script')[0];
+        o.async=1; o.src=t; o.id='fathom-script';
+        m.parentNode.insertBefore(o,m)
+      })(document, window, '//stats.danroc.dev/tracker.js', 'fathom');
+      fathom('set', 'siteId', 'XSOKB');
+      fathom('trackPageview');`
     ]
   ],
   configureWebpack: config => {
